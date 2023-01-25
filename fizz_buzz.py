83bd885785090653a5ed8fdc536be2ba62b1b94b
rk <rebeccakruk@outlook.com>
2023-01-24 20:32:36 -0700

    started assignment 19A

---
+++ fizz_buzz.py
@@ -0,0 +1,14 @@
+def fizz_buzz(list)
+    for num in nums:
+        if (num % 3 == 0):
+            print('Fizz')
+        if (num % 5 == 0):
+            print('Buzz')
+        if (num % 3 != 0) and (num % 3 != 0):
+            print('Fizz Buzz!')
+        
+
+
+
+
+nums = [1,4,6,14,15,12,25,30,45,71,77,264,180,75,199,1560]
